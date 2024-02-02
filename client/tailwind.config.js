4622d20cc58a884f71e180cbfd8ba1dd8aee2ba1
Ammen1 <amenguda@gmail.com>
2024-02-02 21:25:16 +0300

    install react tailwind create templates

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
-}
-
+  plugins: [require("flowbite/plugin"), require("tailwind-scrollbar")],
+};
