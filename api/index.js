6e00e5ea3927ee697303e0e821f9d7dcf2fa2ade
Ammen1 <amenguda@gmail.com>
2024-02-03 00:12:20 +0300

    create and run server

--- api/index.js
+++ api/index.js
@@ -3,5 +3,5 @@ import express from "express";
 const app = express();
 
 app.listen(3000, () => {
-  console.log("server runing on port: 3000");
+  console.log("server runing on portd 3000..!");
 });
