7c38d78c3734b86df195cc5051c0ceb1bb5ab71a
Ammen1 <amenguda@gmail.com>
2024-02-02 23:50:21 +0300

    install init and express create index file for server

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,60 @@
+import { Button, Navbar, TextInput } from "flowbite-react";
+import { Link, useLocation } from "react-router-dom";
+import { AiOutlineSearch } from "react-icons/ai";
+import { FaMoon, FaSun } from "react-icons/fa";
+
+const Header = () => {
+  const path = useLocation().pathname;
+
+  return (
+    <div>
+      <Navbar className="border-b-2">
+        <Link
+          to="/"
+          className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white"
+        >
+          <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
+            Amen
+          </span>
+          Blog
+        </Link>
+        <form>
+          <TextInput
+            type="text"
+            placeholder="Search..."
+            className="hidden lg:inline"
+          />
+
+          <AiOutlineSearch />
+        </form>
+        <Button className="w-12 h-10 hidden sm:inline" color="gray" pill>
+          <span>
+            <FaSun />
+            <FaMoon />
+          </span>
+        </Button>
+        <div className="flex gap-2 md:order-2">
+          <Link to="/sign-in">
+            <Button gradientDuoTone="purpleToBlue" outline>
+              Sign In
+            </Button>
+          </Link>
+          <Navbar.Toggle />
+        </div>
+        <Navbar.Collapse>
+          <Navbar.Link active={path === "/"} as={"div"}>
+            <Link to="/">Home</Link>
+          </Navbar.Link>
+          <Navbar.Link active={path === "/about"} as={"div"}>
+            <Link to="/about">About</Link>
+          </Navbar.Link>
+          <Navbar.Link active={path === "/projects"} as={"div"}>
+            <Link to="/projects">Projects</Link>
+          </Navbar.Link>
+        </Navbar.Collapse>
+      </Navbar>
+    </div>
+  );
+};
+
+export default Header;
