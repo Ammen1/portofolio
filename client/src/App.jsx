5220436a806dad879ab96a73729bfd9dd2644946
Ammen1 <amenguda@gmail.com>
2024-02-03 12:30:38 +0300

    Create footer pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,8 @@ import Dashboard from "./pages/Dashboard";
 import Projects from "./pages/Projects";
 import SignUp from "./pages/SignUp";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
+
 
 export default function App() {
   return (
@@ -19,6 +21,7 @@ export default function App() {
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/projects" element={<Projects />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 }
