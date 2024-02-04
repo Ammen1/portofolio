aa4509c094fcf38bff33aceae3780f9dc72a34ee
Ammen1 <amenguda@gmail.com>
2024-02-04 12:21:43 +0300

    Create Aouth ScrollToTop and ThemeProvider

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,13 +5,16 @@ import SignIn from "./pages/SignIn";
 import Dashboard from "./pages/Dashboard";
 import Projects from "./pages/Projects";
 import SignUp from "./pages/SignUp";
+import Search from "./pages/Search";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-
+import ScrollToTop from "./components/ScrollToTop";
+import PrivateRoute from "./components/PrivateRoute";
 
 export default function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
@@ -19,7 +22,11 @@ export default function App() {
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/search" element={<Search />} />
         <Route path="/projects" element={<Projects />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Route>
       </Routes>
       <Footer />
     </BrowserRouter>
