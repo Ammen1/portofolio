395aa3da06d1e6d76c48298500c1fe57f9c9bb3a
Ammen1 <amenguda@gmail.com>
2024-02-06 23:48:24 +0300

    route for postupadet

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -7,5 +7,6 @@ const router = express.Router();
 router.post("/create", verifyToken, create);
 router.get("/getposts", getposts);
 router.delete("/deletepost/:postId/:userId", verifyToken, deletepost);
+router.put("/updatepost/:postId/:userId", verifyToken, updatepost);
 
 export default router;
