b5c08b688dd8063f5c19e5c8c6a8680e381b73b0
Ammen1 <amenguda@gmail.com>
2024-02-06 23:27:51 +0300

    get all posts functionality  add to post.controller

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -3,22 +3,69 @@ import Post from "../models/post.model.js";
 
 export const create = async (req, res, next) => {
   if (!req.user.isAdmin) {
-    return next(errorHandler(404, "You are not allowed to create post"));
+    return next(errorHandler(403, "You are not allowed to create a post"));
   }
-  if (!req.body.title || req.body.content) {
-    return next(errorHandler(404, "Please provide all required fields"));
+  if (!req.body.title || !req.body.content) {
+    return next(errorHandler(400, "Please provide all required fields"));
   }
-  const slug = req.body.title.split(" ").jon("-");
-  toLowerCase().replace(/[^a-zA=Z0-9-]/g, "");
+  const slug = req.body.title
+    .split(" ")
+    .join("-")
+    .toLowerCase()
+    .replace(/[^a-zA-Z0-9-]/g, "");
   const newPost = new Post({
     ...req.body,
     slug,
     userId: req.user.id,
   });
-
   try {
     const savedPost = await newPost.save();
-    req.status(201).json(savedPost);
+    res.status(201).json(savedPost);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getposts = async (req, res, next) => {
+  try {
+    const startIndex = parseInt(req.query.startIndex) || 0;
+    const limit = parseInt(req.query.limit) || 9;
+    const sortDirection = req.query.order === "asc" ? 1 : -1;
+    const posts = await Post.find({
+      ...(req.query.userId && { userId: req.query.userId }),
+      ...(req.query.category && { category: req.query.category }),
+      ...(req.query.slug && { slug: req.query.slug }),
+      ...(req.query.postId && { _id: req.query.postId }),
+      ...(req.query.searchTerm && {
+        $or: [
+          { title: { $regex: req.query.searchTerm, $options: "i" } },
+          { content: { $regex: req.query.searchTerm, $options: "i" } },
+        ],
+      }),
+    })
+      .sort({ updatedAt: sortDirection })
+      .skip(startIndex)
+      .limit(limit);
+
+    const totalPosts = await Post.countDocuments();
+
+    const now = new Date();
+
+    const oneMonthAgo = new Date(
+      now.getFullYear(),
+      now.getMonth() - 1,
+      now.getDate()
+    );
+
+    const lastMonthPosts = await Post.countDocuments({
+      createdAt: { $gte: oneMonthAgo },
+    });
+
+    res.status(200).json({
+      posts,
+      totalPosts,
+      lastMonthPosts,
+    });
   } catch (error) {
     next(error);
   }
