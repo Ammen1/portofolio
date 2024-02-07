6b5234aae951316d1a548166bc30d977be5febed
Ammen1 <amenguda@gmail.com>
2024-02-07 22:42:48 +0300

    create user crude functionality route for all function

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,21 +1,21 @@
 import express from "express";
 import {
-  //   deleteUser,
-  //   getUser,
-  //   getUsers,
+  deleteUser,
+  getUser,
+  getUsers,
   signout,
   test,
-  //   updateUser,
+  updateUser,
 } from "../controllers/user.controller.js";
-// import { verifyToken } from "../utils/verifyUser.js";
+import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
 
 router.get("/test", test);
-// router.put('/update/:userId', verifyToken, updateUser);
-// router.delete('/delete/:userId', verifyToken, deleteUser);
+router.put("/update/:userId", verifyToken, updateUser);
+router.delete("/delete/:userId", verifyToken, deleteUser);
 router.post("/signout", signout);
-// router.get('/getusers', verifyToken, getUsers);
-// router.get('/:userId', getUser);
+router.get("/getusers", verifyToken, getUsers);
+router.get("/:userId", getUser);
 
 export default router;
