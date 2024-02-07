1197aa850f12ca1fb1d9cae92ac24fdf5f6e45d8
Ammen1 <amenguda@gmail.com>
2024-02-07 22:24:51 +0300

    Creare deleteComment and get all Comments with thier route

--- api/controllers/comment.controller.js
+++ api/controllers/comment.controller.js
@@ -79,3 +79,46 @@ export const editComment = async (req, res, next) => {
     next(error);
   }
 };
+
+export const deleteComment = async (req, rea, next) => {
+  try {
+    const comment = await Comment.findById(req.params.commentId);
+    if (!comment) {
+      return next(errorHandler(404, "Comment not found"));
+    }
+    if (comment.userId !== req.user.id && !req.user.isAdmin) {
+      return next(errorHandler(403, "You are allowed to delete this comment"));
+    }
+    await comment.findByIdAndDelete(req.params.commentId);
+    req.status(200).json("Comment has been deleted");
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getcomments = async (req, res, next) => {
+  if (!req.user.isAdmin) {
+    return next(errorHandler(404, "You are not allowed to get all comments"));
+  }
+  try {
+    const startIndex = parentInt(req.query.startIndex) || 0;
+    const limit = parentInt(req.query.limit) || 9;
+    const sortDirection = req.query.sort === "desc" ? -1 : 1;
+    const comments = (await Comment.find().sort({ createdAt: sortDirection }))
+      .skip(startIndex)
+      .limit(limit);
+    const totalComments = await Comment.countDocuments();
+    const now = new Date();
+    const oneMonthAgo = new Date(
+      now.getFullYear(),
+      now.getMonth() - 1,
+      now.getDate()
+    );
+    const lastMonthComments = await Comment.countDocuments({
+      createdAt: { $gte: oneMonthAgo },
+    });
+    res.status(200).json({ comments, totalComments, lastMonthComments });
+  } catch (error) {
+    next(error);
+  }
+};
--- api/routes/comment.route.js
+++ api/routes/comment.route.js
@@ -13,5 +13,7 @@ router.post("/create", verifyToken, createComment);
 router.get("/getPostComments/:postId", getPostComments);
 router.put("/likeComment/:commentId", verifyToken, likeComment);
 router.put("/editComment/:commentId", verifyToken, editComment);
+router.delete("/deleteComment/:commentId", verifyToken, deleteComment);
+router.get("/getcomments", verifyToken, getcomments);
 
 export default router;
