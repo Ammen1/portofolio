55e4ad59780d62c667f84ef06b512c5e30651267
Ammen1 <amenguda@gmail.com>
2024-02-07 21:48:03 +0300

    create editcommetn and route for comment

--- api/controllers/comment.controller.js
+++ api/controllers/comment.controller.js
@@ -35,7 +35,7 @@ export const getPostComments = async (req, res, next) => {
   }
 };
 
-export const likeComments = async (req, res, next) => {
+export const likeComment = async (req, res, next) => {
   try {
     const comment = await Comment.findById(req.params.commentId);
     if (!comment) {
@@ -51,6 +51,30 @@ export const likeComments = async (req, res, next) => {
     }
     await comment.save();
     res.status(200).json(comment);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const editComment = async (req, res, next) => {
+  try {
+    const comment = await Comment.findById(req.params.commentId);
+    if (!comment) {
+      return next(errorHandler(404, "Comment not found"));
+    }
+    if (comment.userId != req.user.id && !req.user.isAdmin) {
+      return next(
+        errorHandler(404, "You are not allowed to edit this comment")
+      );
+    }
+    const editedComment = await Comment.findByIdAndUpdate(
+      req.params.commentId,
+      {
+        content: req.body.content,
+      },
+      { new: true }
+    );
+    res.status(200).json(editedComment);
   } catch (error) {
     next(error);
   }
