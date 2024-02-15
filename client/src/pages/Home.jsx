d3f4f68f2779ba4af26e588e7342ff2f55b0c910
Ammen1 <amenguda@gmail.com>
2024-02-16 01:39:43 +0300

    create skills show page ui

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,6 +2,8 @@ import { Link } from "react-router-dom";
 import CallToAction from "../components/CallToAction";
 import { useEffect, useState } from "react";
 import PostCard from "../components/PostCard";
+import Hero from "../components/Hero";
+import Contact from "./Contact";
 
 export default function Home() {
   const [posts, setPosts] = useState([]);
@@ -15,23 +17,19 @@ export default function Home() {
     fetchPosts();
   }, []);
   return (
-    <div>
-      <div className="flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto ">
-        <h1 className="text-3xl font-bold lg:text-6xl">Welcome to my Blog</h1>
-        <p className="text-gray-500 text-xs sm:text-sm">
-          Here you'll find a variety of articles and tutorials on topics such as
-          web development, software engineering, and programming languages.
-        </p>
-        <Link
-          to="/search"
-          className="text-xs sm:text-sm text-teal-500 font-bold hover:underline"
-        >
-          View all posts
-        </Link>
-      </div>
+    <div className="">
+      <Hero />
+
+      <Link
+        to="/search"
+        className="text-xs sm:text-sm text-teal-500 font-bold hover:underline"
+      >
+        View all posts
+      </Link>
       <div className="p-3 bg-amber-100 dark:bg-slate-700">
         <CallToAction />
       </div>
+      <Contact />
 
       <div className="max-w-6xl mx-auto p-3 flex flex-col gap-8 py-7">
         {posts && posts.length > 0 && (
