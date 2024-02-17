7cff9663fcee5fc9e314d1a8e8c55547704db1aa
Ammen1 <amenguda@gmail.com>
2024-02-17 18:05:33 +0300

    config tailwind add fontfamily

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -6,8 +6,18 @@ export default {
     "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    fontFamily: {
+      Poppins: "Poppins",
+      Paprika: "Paprika",
+      Inria: "Inria Serif",
+    },
+    container: {
+      center: true,
+      padding: "4rem",
+    },
     extend: {},
   },
+
   // eslint-disable-next-line no-undef
   plugins: [require("flowbite/plugin"), require("tailwind-scrollbar")],
 };
