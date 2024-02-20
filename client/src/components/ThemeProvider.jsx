899a35f7e3b31d83c4e7bf363c8f65900f772f5e
Ammen1 <amenguda@gmail.com>
2024-02-20 15:31:34 +0300

    chnage bg-gradound color to gray-50

--- client/src/components/ThemeProvider.jsx
+++ client/src/components/ThemeProvider.jsx
@@ -4,7 +4,7 @@ export default function ThemeProvider({ children }) {
   const { theme } = useSelector((state) => state.theme);
   return (
     <div className={theme}>
-      <div className="bg-white text-gray-700 dark:text-gray-200 dark:bg-[#06223F] min-h-screen">
+      <div className=" bg-gray-50 text-gray-700 dark:text-gray-200 dark:bg-[#06223F] min-h-screen">
         {children}
       </div>
     </div>
