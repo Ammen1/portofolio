e17bad9fb9580281cac8d8dd8e87ff9b7120b008
Tamirat <amenguda@gmail.com>
2024-03-03 01:46:09 +0300

    romove the link

--- api/index.js
+++ api/index.js
@@ -25,7 +25,7 @@ const app = express();
 
 app.use(cors(
   {
-    origin: [ "https://protofolio-alpha.vercel.app/" ],
+    origin: [ "" ],
     methods: [ "POST",  "GET" ],
     Credential: true
   }
