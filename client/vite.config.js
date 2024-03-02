d6f06b40acbfed15ba4037ebc16ea2f5f028692e
Tamirat <amenguda@gmail.com>
2024-03-03 01:39:46 +0300

    push vite cofing file

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: "portofolio-api-five.vercel.app",
         secure: false,
       },
     },
