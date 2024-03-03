67d26170a884b2af16012484bfe664f7f67b4ffe
Tamirat <amenguda@gmail.com>
2024-03-03 03:40:37 +0300

    push

--- api/index.js
+++ api/index.js
@@ -1,58 +1,49 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import userRoutes from './routes/user.route.js';
-import authRoutes from './routes/auth.route.js';
-import postRoutes from './routes/post.route.js';
-import commentRoutes from './routes/comment.route.js';
+import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import path from 'path';
 
 dotenv.config();
 
-mongoose
-  .connect(process.env.MONGO)
+mongoose.connect(process.env.MONGO, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log('MongoDb is connected');
   })
   .catch((err) => {
-    console.log(err);
+    console.error(err);
   });
 
 const __dirname = path.resolve();
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-app.use(cors(
-  {
-    origin: [ "portofolio-api-silk.vercel.app" ],
-    methods: [ "POST",  "GET" ],
-    Credential: true
-  }
-));
+app.use(cors({
+  origin: ["https://portofolio-api-silk.vercel.app"],
+  methods: ["POST", "GET"],
+  credentials: true,
+}));
 
 app.use(express.json());
 app.use(cookieParser());
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000!');
-});
-app.get('/', (req, res) => {
-  res.send("Hello amen");
-});
+// Serve the React app
+app.use(express.static(path.join(__dirname, 'client/dist')));
 
+// API routes
+app.use('/api/user', require('./routes/user.route.js'));
+app.use('/api/auth', require('./routes/auth.route.js'));
+app.use('/api/post', require('./routes/post.route.js'));
+app.use('/api/comment', require('./routes/comment.route.js'));
 
-app.use('/api/user', userRoutes);
-app.use('/api/auth', authRoutes);
-app.use('/api/post', postRoutes);
-app.use('/api/comment', commentRoutes);
-
-app.use(express.static(path.join(__dirname, '/client/dist')));
-
+// Catch-all route to serve the React app
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+  res.sendFile(path.join(__dirname, 'client/dist', 'index.html'));
 });
 
+// Error handling middleware
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || 'Internal Server Error';
@@ -61,4 +52,8 @@ app.use((err, req, res, next) => {
     statusCode,
     message,
   });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
