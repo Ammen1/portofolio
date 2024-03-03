d75f2659c5bee91004e1bbec30ed2d6a8fe92cf1
Tamirat <amenguda@gmail.com>
2024-03-03 03:53:38 +0300

    update index.js

--- api/index.js
+++ api/index.js
@@ -21,7 +21,7 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(cors({
-  origin: [""],
+  origin: ["https://tamirat-portofolio.vercel.app"],
   methods: ["POST", "GET"],
   credentials: true,
 }));
